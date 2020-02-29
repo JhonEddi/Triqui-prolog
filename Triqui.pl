3e17102e9605661feafda25c762b1d74bc6b5376
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2020-02-29 17:32:13 -0500

    Add files via upload

---
+++ Triqui.pl
@@ -0,0 +1,8 @@
+gana("fila",1,2,3).
+gana("fila",4,5,6).
+gana("fila",7,8,9).
+gana("columna",1,4,7).
+gana("columna",2,5,8).
+gana("columna",3,6,9).
+gana("diagonal",1,5,9).
+gana("diagonal",3,5,7).
