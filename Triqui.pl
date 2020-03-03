66deab1a0a053f2b9539b8cd388fec7eaa879198
Forson666 <51457747+Forson666@users.noreply.github.com>
2020-03-03 13:46:46 -0500

    Update Triqui.pl

--- Triqui.pl
+++ Triqui.pl
@@ -1,8 +1,29 @@
-gana("fila",1,2,3).
-gana("fila",4,5,6).
-gana("fila",7,8,9).
-gana("columna",1,4,7).
-gana("columna",2,5,8).
-gana("columna",3,6,9).
-gana("diagonal",1,5,9).
-gana("diagonal",3,5,7).
+
+%estados validos de las casillas
+estado(o).
+estado(x).
+vacio(v).
+
+%valor inicial de las casillas (vacio)
+iniciar([v,v,v,v,v,v,v,v,v]).
+
+gana([E,E,E,_,_,_,_,_,_]):-estado(E).
+gana([_,_,_,E,E,E,_,_,_]):-estado(E).
+gana([_,_,_,_,_,_,E,E,E]):-estado(E).
+
+gana([E,_,_,E,_,_,E,_,_]):-estado(E).
+gana([_,E,_,_,E,_,_,E,_]):-estado(E).
+gana([_,_,E,_,_,E,_,_,E]):-estado(E).
+
+gana([E,_,_,_,E,_,_,_,E]):-estado(E).
+gana([_,_,E,_,E,_,E,_,_]):-estado(E).
+
+estado(1,[X|_],X).
+estado(P,[_|Xs],E):-P1 is P-1,estado(P1,Xs,E).
+
+libre(P,Tablero):-estado(P,Tablero,v).
+
+insertar(E,1,[_|Xs],[E,Xs]).
+insertar(E,P,[X|Xs],[X|Xs1]):-P1 is P-1,insertar(E,P1,Xs,[X|Xs1]).
+
+movimiento(P,E,Tablero,Tablero1):-estado(N,Tablero,v),insertar(E,P,Tablero,Tablero1).
